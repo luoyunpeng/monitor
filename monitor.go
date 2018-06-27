85bf0f52528a389f463e76d9de0675111fd2e380
luoyunpeng <956941328@qq.com>
2018-06-27 09:59:10 +0800

    add container info api

--- monitor.go
+++ monitor.go
@@ -36,6 +36,7 @@ func main() {
 	v1 := router.Group("")
 
 	v1.GET("/container/stats/:id", ContainerStats)
+	v1.GET("/container/info", ContainerInfo)
 	v1.GET("/container/logs/:id", ContainerLogs)
 	v1.GET("/host/mem", HostMemInfo)
 
