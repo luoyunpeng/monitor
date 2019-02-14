1f6e2762ac5c8dc72827e9a509ea2181794979d6
luoyunpeng <956941328@qq.com>
2019-02-14 09:13:25 +0800

    str2bytes add

--- container/stats_tools.go
+++ container/stats_tools.go
@@ -277,3 +277,9 @@ func IsKnownHost(host string) bool {
 func Bytes2str(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
+
+func Str2bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
